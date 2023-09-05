15a166a665356d305e1b662389eae6705471a329
Ross <rosshamey@gmail.com>
2023-09-05 11:08:26 -0400

    1.7

---
+++ Module 1/CSE163_P1-7.py
@@ -0,0 +1,8 @@
+# Ross Hamey
+# CSE163 - P1.7
+# 9/5/2023
+# https://github.com/Endeared
+
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+print(f'{user_name} is {user_age} years old.')
