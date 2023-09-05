42a6b02571310b8ccfba062b1f99ca92207f0cd9
Ross <rosshamey@gmail.com>
2023-09-05 11:06:03 -0400

    1.5

--- Module 1/CSE163_P1-5.py
+++ Module 1/CSE163_P1-5.py
@@ -3,3 +3,7 @@
 # 9/5/2023
 # https://github.com/Endeared
 
+base = float(input("Enter the base: "))
+height = float(input("Enter the height: "))
+area = base * height * 0.5
+print("The area is", area, "square units.")
