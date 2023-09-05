c8d4c41021154a8ccdfe97a6fec7439574de6905
Ross <rosshamey@gmail.com>
2023-09-05 11:03:51 -0400

    1.6 and 1.2

---
+++ Module 1/CSE163_P1-2.py
@@ -0,0 +1,12 @@
+# Ross Hamey
+# CSE163 - P1.6
+# 9/5/2023
+# https://github.com/Endeared
+
+my_name = "Ross Hamey"
+my_address = "5781 West Elkton Road, Somerville, OH 45064"
+my_number = "513-562-7257"
+
+print(my_name)
+print(my_address)
+print(my_number)
---
+++ Module 1/CSE163_P1-6.py
@@ -0,0 +1,8 @@
+# Ross Hamey
+# CSE163 - P1.6
+# 9/5/2023
+# https://github.com/Endeared
+
+radius = float(input("Please enter a radius: "))
+area = 3.14 * (radius * radius)
+print(f'The area is {area} square units.')
